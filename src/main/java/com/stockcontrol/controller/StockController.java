3bce4466f8feefbcb1f01b7a07cab003dec08378
MrCaste <pedrojcaste@gmail.com>
2024-12-28 21:28:30 +0000

    Actualizacion filtro y fecha

--- src/main/java/com/stockcontrol/controller/StockController.java
+++ src/main/java/com/stockcontrol/controller/StockController.java
@@ -2,21 +2,31 @@ package com.stockcontrol.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
 
 import com.stockcontrol.modelo.Product;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 public class StockController implements Initializable {
 
     @FXML
     private TableColumn<Product, Integer> tablaIdentificador;
+
+    @FXML
+    private TableColumn<Product, String> tablaFecha;
 
     @FXML
     private TableView<Product> tableView;
@@ -28,6 +38,9 @@ public class StockController implements Initializable {
     private TableColumn<Product, Integer> tablaStock;
 
     @FXML
+    private DatePicker datePicker;
+
+    @FXML
     private TextField textFieldIdentificador;
 
     @FXML
@@ -37,6 +50,12 @@ public class StockController implements Initializable {
     private TextField textFieldStock;
 
     @FXML
+    private TextField textFieldFiltro;
+
+    private ObservableList<Product> list = FXCollections.observableArrayList();
+    private FilteredList<Product> filterList;
+
+    @FXML
     void addStock(ActionEvent event) {
         String textIdentificator = textFieldIdentificador.getText();
         String textProduct = textFieldProducto.getText();
@@ -44,8 +63,8 @@ public class StockController implements Initializable {
         if(textStock.isEmpty() || textProduct.isEmpty() || textIdentificator.isEmpty()){
             ControllerTransitions.showErrorAlert("Error", "Todos los campos son requeridos");
         } else {
-            Product product = new Product(Integer.parseInt(textFieldIdentificador.getText()), textFieldProducto.getText(), Integer.parseInt(textFieldStock.getText()));
-            tableView.getItems().add(product);
+            Product product = new Product(Integer.parseInt(textFieldIdentificador.getText()), textFieldProducto.getText(), Integer.parseInt(textFieldStock.getText()), datePicker.getValue());
+            list.add(product);
         }
         textFieldIdentificador.clear();
         textFieldProducto.clear();
@@ -63,10 +82,15 @@ public class StockController implements Initializable {
     void modifyStock(ActionEvent event) {
         Product product = tableView.getItems().get(tableView.getSelectionModel().getSelectedIndex());
         int index = tableView.getSelectionModel().getSelectedIndex();
-        Product productModify = new Product(product.getIdentificator(), textFieldProducto.getText(), Integer.parseInt(textFieldStock.getText()));
-        tableView.getItems().set(index, productModify);
+        Product productModify = new Product(product.getIdentificator(), textFieldProducto.getText(), Integer.parseInt(textFieldStock.getText()), datePicker.getValue());
+        list.set(index, productModify);
         textFieldProducto.clear();
         textFieldStock.clear();
+    }
+
+    @FXML
+    void filterProduct(KeyEvent event) {
+        filterList.setPredicate(product -> filter(product));
     }
 
     @Override
@@ -75,6 +99,17 @@ public class StockController implements Initializable {
         tablaIdentificador.setCellValueFactory(new PropertyValueFactory<Product, Integer>("identificator"));
         tablaProducto.setCellValueFactory(new PropertyValueFactory<Product, String>("name"));
         tablaStock.setCellValueFactory(new PropertyValueFactory<Product, Integer>("stock"));
+        tablaFecha.setCellValueFactory(new PropertyValueFactory<Product, String>("fecha"));
+        filterList = new FilteredList<>(list, predicate -> true);
+        tableView.setItems(filterList); 
+    }
+
+    public boolean filter(Product product){
+        String filterText = textFieldFiltro.getText();
+        if(filterText.isEmpty()){
+            return true;
+        }
+        return String.valueOf(product.getIdentificator()).toLowerCase().contains(filterText.toLowerCase());
     }
 
 }
--- src/main/java/com/stockcontrol/modelo/Product.java
+++ src/main/java/com/stockcontrol/modelo/Product.java
@@ -1,16 +1,22 @@
 package com.stockcontrol.modelo;
 
+import java.time.LocalDate;
+
 public class Product {
 
     private String name;
+    private String fecha;
     private int identificator;
     private int stock;
     
-    public Product(int identificator, String name, int stock) {
+    public Product(int identificator, String name, int stock, LocalDate fecha) {
         this.name = name;
         this.identificator = identificator;
         this.stock = stock;
+        this.fecha = fecha.toString();
     }
+
+    
 
     public String getName() {
         return name;
@@ -34,5 +40,13 @@ public class Product {
 
     public void setStock(int stock) {
         this.stock = stock;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
     }
 }
